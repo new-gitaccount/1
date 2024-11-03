48c74eb4799844c3193bef708231e76d7d0945e8
new-gitaccount <abdullohfazliddinov2008@gmail.com>
2024-11-03 20:05:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+matn = "   Salom, Dunyo!   "
+
+print(matn)
+
+bosh_harf = matn.upper()  
+print(bosh_harf)
+
+kichik_harf = matn.lower()  
+print(kichik_harf)
+
+
+birinchi_harf = matn.capitalize() 
+print(birinchi_harf)
+
+
+boshliq_tasfiyalangan = matn.strip()  
+print(boshliq_tasfiyalangan)
+
+
+sarlavha = matn.title()  
+print(sarlavha)
